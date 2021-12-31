7ffbed4b016ffcc6259e4e6217bcfbb1331817ee
nitishsinghchauhan <nitishchauhan018@gmail.com>
2021-12-31 17:10:56 +0530

    events done

--- app/src/main/java/com/example/alcheringa2022/Home.kt
+++ app/src/main/java/com/example/alcheringa2022/Home.kt
@@ -11,24 +11,30 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.materialIcon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import com.example.alcheringa2022.databinding.FragmentHomeBinding
 import com.example.alcheringa2022.ui.theme.Alcheringa2022Theme
 import com.example.alcheringa2022.ui.theme.clash
+import com.example.alcheringa2022.ui.theme.hk_grotesk
+import org.intellij.lang.annotations.JdkConstants
 
 /**
  * A simple [Fragment] subclass.
@@ -47,20 +53,20 @@ class Home : Fragment() {
         "JUBIN NAUTIYAL",
         "Pro Nights",
         "12 Feb, 4 PM",
-        "Online", R.drawable.jubin
+        "ONLINE", R.drawable.jubin
     ),
 
         eventdetail(
             "DJ SNAKE",
             "Pro Nights",
             "11 Feb, 4 PM",
-            "Online", R.drawable.djsnake
+            "ON GROUND", R.drawable.djsnake
         ),
         eventdetail(
             "TAYLOR SWIFT",
         "Pro Nights",
         "13 Feb, 4 PM",
-        "Online", R.drawable.taylor
+        "ON GROUND", R.drawable.taylor
     )
 
 
@@ -93,17 +99,33 @@ class Home : Fragment() {
         binding.compose1.setContent {
             Alcheringa2022Theme() {
 
-
-                LazyRow(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(15.dp)
-                ) {
-                    items(events) { dataeach -> upcomingEvents(eventdetail = dataeach) }
+                Box(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)) {
+                    LazyRow(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        items(events) { dataeach -> ongoingEvents(eventdetail = dataeach) }
+                    }
                 }
+                
+                
+
+//                Box(modifier = Modifier
+//                    .fillMaxWidth()
+//                    .padding(20.dp)) {
+//                    LazyRow(
+//                        modifier = Modifier.fillMaxWidth(),
+//                        horizontalArrangement = Arrangement.spacedBy(12.dp)
+//                    ) {
+//                        items(events) { dataeach -> upcomingEvents(eventdetail = dataeach) }
+//                    }
+//                }
+
 
 
             }
-
         }
     }
 
@@ -131,9 +153,9 @@ class Home : Fragment() {
             return fragment
         }
     }
-
+    @Preview
     @Composable
-    fun upcomingEvents(eventdetail: eventdetail) {
+    fun upcomingEvents(eventdetail: eventdetail= events[1]) {
 
         Box() {
 
@@ -159,10 +181,26 @@ class Home : Fragment() {
                             .padding(12.dp), contentAlignment = Alignment.BottomStart){
                             Column {
                                 Text(text = eventdetail.artist, style = MaterialTheme.typography.h1)
-                                Spacer(modifier = Modifier.height(10.dp))
-                                Text(text = eventdetail.time, style = TextStyle(color = colorResource(id = R.color.textGray),fontFamily = clash,fontWeight = FontWeight.Normal,fontSize = 14.sp))
-
-
+                                Spacer(modifier = Modifier.height(2.dp))
+                                Text(text = eventdetail.category, style = TextStyle(color = colorResource(id = R.color.textGray),fontFamily = clash,fontWeight = FontWeight.W600,fontSize = 14.sp))
+                                Spacer(modifier = Modifier.height(8.dp))
+                                Text(text = eventdetail.time, style = TextStyle(color = colorResource(id = R.color.textGray),fontFamily = hk_grotesk,fontWeight = FontWeight.Normal,fontSize = 14.sp))
+                                Spacer(modifier = Modifier.height(2.dp))
+                                Row {
+                                    Box(modifier = Modifier
+                                        .height(20.dp)
+                                        .width(20.dp)) {
+                                        Image(
+                                            painter = if (eventdetail.mode.contains("ONLINE")) {
+                                                painterResource(id = R.drawable.online)
+                                            } else {
+                                                painterResource(id = R.drawable.onground)
+                                            },
+                                            contentDescription = null, modifier = Modifier.fillMaxSize()
+                                        )
+                                    }
+                                    Spacer(modifier = Modifier.width(4.dp))
+                                    Text(text = eventdetail.mode,style = TextStyle(color = colorResource(id = R.color.textGray),fontFamily = hk_grotesk,fontWeight = FontWeight.Normal,fontSize = 14.sp))
                             }
                         }
 
@@ -171,6 +209,81 @@ class Home : Fragment() {
 
             }
         }
+    }
+
+
+
+}
+
+    @Preview
+    @Composable
+    fun ongoingEvents(eventdetail: eventdetail= events[1]) {
+
+        Box() {
+
+            Card(modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp),
+                elevation = 5.dp) {
+                Box(modifier = Modifier
+                    .height(256.dp)
+                    .width(218.dp)){
+                    Image(painter = painterResource(id = eventdetail.imgurl), contentDescription = "artist", contentScale = ContentScale.Crop)
+                    Box(modifier = Modifier
+                        .fillMaxWidth()
+                        .height(21.dp)
+                        .background(
+                            color = colorResource(
+                                id = R.color.ThemeRed
+                            )
+                        )
+                    ){ Text(text = "⬤ LIVE", color = Color.White, modifier = Modifier.align(alignment = Alignment.Center), fontSize = 12.sp)}
+                    Box(modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(
+                                    Color.Transparent,
+                                    Color.Black
+                                ), startY = 300f
+                            )
+                        ))
+                    Box(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(12.dp), contentAlignment = Alignment.BottomStart){
+                        Column {
+                            Text(text = eventdetail.artist, style = MaterialTheme.typography.h1)
+                            Spacer(modifier = Modifier.height(2.dp))
+                            Text(text = eventdetail.category, style = TextStyle(color = colorResource(id = R.color.textGray),fontFamily = clash,fontWeight = FontWeight.W600,fontSize = 14.sp))
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(text = eventdetail.time, style = TextStyle(color = colorResource(id = R.color.textGray),fontFamily = hk_grotesk,fontWeight = FontWeight.Normal,fontSize = 14.sp))
+                            Spacer(modifier = Modifier.height(2.dp))
+                            Row {
+                                Box(modifier = Modifier
+                                    .height(20.dp)
+                                    .width(20.dp)) {
+                                    Image(
+                                        painter = if (eventdetail.mode.contains("ONLINE")) {
+                                            painterResource(id = R.drawable.online)
+                                        } else {
+                                            painterResource(id = R.drawable.onground)
+                                        },
+                                        contentDescription = null, modifier = Modifier.fillMaxSize()
+                                    )
+                                }
+                                Spacer(modifier = Modifier.width(4.dp))
+                                Text(text = eventdetail.mode,style = TextStyle(color = colorResource(id = R.color.textGray),fontFamily = hk_grotesk,fontWeight = FontWeight.Normal,fontSize = 14.sp))
+                            }
+                        }
+
+                    }
+
+
+                }
+            }
+        }
+
+
+
     }
 }
 
