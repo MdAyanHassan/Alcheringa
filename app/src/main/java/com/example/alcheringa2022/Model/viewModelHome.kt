937bf7365dc2d6c5a108a3b226e1913fa6b3afb9
nitishsinghchauhan <nitishchauhan018@gmail.com>
2022-01-24 12:12:46 +0530

    viewmodel initiated

--- app/src/main/java/com/example/alcheringa2022/Model/viewModelHome.kt
+++ app/src/main/java/com/example/alcheringa2022/Model/viewModelHome.kt
@@ -1,8 +1,24 @@
 package com.example.alcheringa2022.Model
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
+import com.example.alcheringa2022.eventdetail
+import com.google.firebase.abt.FirebaseABTesting
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class viewModelHome: ViewModel() {
+    val fb = FirebaseFirestore.getInstance()
+    val topEvents= mutableStateListOf<eventdetail>()
+    val allEvents= mutableStateListOf<eventdetail>()
+    val ownEvents= mutableStateListOf<eventdetail>()
+
+    fun getAllEvents(){
+
+        fb.collection("")
+    }
+
+
 
 
 
