ac2a5e2cf6ff3323a06bad795b6d027575a99796
sg-iitg <shreya.goel@iitg.ac.in>
2022-01-24 23:44:17 +0530

    Added Edit Image in Profile Page

--- app/src/main/java/com/example/alcheringa2022/Login.java
+++ app/src/main/java/com/example/alcheringa2022/Login.java
@@ -77,7 +77,7 @@ public class Login extends AppCompatActivity {
         SignuptextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                startActivity(new Intent(getApplicationContext(),SignUp.class));
             }
         });
     }
--- app/src/main/java/com/example/alcheringa2022/ProfilePage.java
+++ app/src/main/java/com/example/alcheringa2022/ProfilePage.java
@@ -1,14 +1,162 @@
 package com.example.alcheringa2022;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.example.alcheringa2022.Model.YourOrders_model;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Source;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
+
 public class ProfilePage extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+
+    TextView name;
+    ImageView user_dp;
+    ImageView edit_dp_button;
+    Button save_button;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firestore;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+
+    private Uri filePath;
+    private final int PICK_IMAGE_REQUEST = 71;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        name = findViewById(R.id.user_name);
+        user_dp = findViewById(R.id.profile_image);
+        edit_dp_button = findViewById(R.id.edit_dp_button);
+        save_button = findViewById(R.id.SaveBtn);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        firestore= FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        sharedPreferences = getSharedPreferences("USER",MODE_PRIVATE);
+
+        fill_user_details();
+
+        edit_dp_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+
+        save_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+    }
+
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                user_dp.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void fill_user_details()
+    {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        assert user != null;
+        String email = user.getEmail();
+        assert email != null;
+
+        firestore.collection("USERS").document(email).get().addOnCompleteListener(task -> {
+            if(task.isSuccessful() && task.getResult() != null){
+                name.setText(task.getResult().getString("Name"));
+                //sharedPreferences.getString("Name", "");
+                Glide.with(this).load(task.getResult().getString("PhotoURL")).into(user_dp);
+            }else{
+                //
+            }
+        });
+    }
+
+    private void uploadImage() {
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        assert user != null;
+        String email = user.getEmail();
+        assert email != null;
+
+        if(filePath != null)
+        {
+            StorageReference ref = storageReference.child("Users/"+ email);
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Log.d("Image", "Uploaded!!");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("Image", "Failed!!");
+                        }
+                    });
+        }
     }
 }
