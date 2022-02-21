cb8e6b3c9358bbc1264f5eb0269ed66d9369644c
Atharvat <tagalpallewaratharva@gmail.com>
2022-02-22 00:06:54 +0530

    commit

---
+++ app/src/main/java/com/alcheringa/alcheringa2022/SizeChartActivity.kt
@@ -0,0 +1,11 @@
+package com.alcheringa.alcheringa2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SizeChartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_size_chart)
+    }
+}
