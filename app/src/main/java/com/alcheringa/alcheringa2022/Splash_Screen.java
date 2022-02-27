d21419d4deb084c24c8c3745d45c4244dd190cf3
Vipinjaluthria <vipin19@iitg.ac.in>
2022-02-27 15:17:35 +0530

    splsh screen added

--- app/src/main/java/com/alcheringa/alcheringa2022/Splash_Screen.java
+++ app/src/main/java/com/alcheringa/alcheringa2022/Splash_Screen.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.widget.VideoView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -16,17 +18,18 @@ import java.util.Objects;
 public class Splash_Screen extends AppCompatActivity {
     SharedPreferences sharedPreferences;
     FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        Objects.requireNonNull(getSupportActionBar()).hide();
-        firebaseAuth=FirebaseAuth.getInstance();
-        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
-        sharedPreferences=getSharedPreferences("USER",MODE_PRIVATE);
-        boolean isLoggedIn=sharedPreferences.getBoolean("isLoggedIn",false);
-
+        videoView=findViewById(R.id.videoview);
+        loadVideo();
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        sharedPreferences = getSharedPreferences("USER", MODE_PRIVATE);
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -38,6 +41,42 @@ public class Splash_Screen extends AppCompatActivity {
                     finish();
                 }
             }
-        },3000);
+        },4000);
+
+
+
+    }
+    private void loadVideo() {
+        videoView=findViewById(R.id.videoview);
+        Uri uri= Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.splash);
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+        videoView.setOnPreparedListener(mp -> mp.setLooping(true));
+    }
+
+
+    @Override
+    protected void onResume() {
+        videoView.resume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        videoView.start();
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        videoView.suspend();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        videoView.stopPlayback();
+        super.onDestroy();
     }
 }
