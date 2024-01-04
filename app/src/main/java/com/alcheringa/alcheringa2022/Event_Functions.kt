37abd5aed30225890dce2671833008b92f55df53
iamrupayandaripa <rupayandaripa@gmail.com>
2024-01-04 16:48:06 +0530

    MerchFragmentCompose.kt page is changed

--- app/src/main/java/com/alcheringa/alcheringa2022/Event_Functions.kt
+++ app/src/main/java/com/alcheringa/alcheringa2022/Event_Functions.kt
@@ -77,7 +77,7 @@ fun Event_card_Scaffold(
             .wrapContentWidth()
             .border(
                 2.dp,
-                color = lighterGreen,
+                color = selectionGreen,
                 RoundedCornerShape(4.dp)
             )
     } else {
@@ -461,7 +461,7 @@ fun Event_card_Scaffold(
                                 if(eventdetail.isLive.value){
                                     Text(
                                         text = "Live ",
-                                        color = lighterGreen,
+                                        color = selectionGreen,
                                         fontSize = 14.sp,
                                         fontFamily = futura
                                     )
